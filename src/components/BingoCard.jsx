a4f0cad44112691ca07397469139a87bfcc2c5b8
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-12-03 14:12:18 +0000

    Actualizar estilos de BingoCard y BingoCell; corregir asignación de userBingoCard y añadir transición en BingoCell

--- src/components/BingoCard.jsx
+++ src/components/BingoCard.jsx
@@ -12,8 +12,8 @@ const BingoCard = () => {
   console.log(gameState);
   console.log(userId);
   
-  const userBingoCard = {"b7eb26d5-bbf3-44ed-9f47-0c5e43b5ba64":[[1,12,10,5,15],[22,25,16,24,20],[44,36,"FREE",31,38],[49,46,60,55,48],[74,66,63,75,69]]}["b7eb26d5-bbf3-44ed-9f47-0c5e43b5ba64"];
-  // const userBingoCard = bingoCards[userId] || []  ;
+  // const userBingoCard = {"b7eb26d5-bbf3-44ed-9f47-0c5e43b5ba64":[[1,12,10,5,15],[22,25,16,24,20],[44,36,"FREE",31,38],[49,46,60,55,48],[74,66,63,75,69]]}["b7eb26d5-bbf3-44ed-9f47-0c5e43b5ba64"];
+  const userBingoCard = bingoCards[userId] || []  ;
   const bingoLetters = ['B', 'I', 'N', 'G', 'O'];
 
   return (
